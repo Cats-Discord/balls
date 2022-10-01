30799be18c4fa9246d424abe971c4d5c530ffe48
Jackson Isaiah <jacksonisiah01@gmail.com>
2022-10-01 17:43:53 -0400

    Change image default loader for static deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
+      extension: /\.mdx?$/,
   })
   module.exports = withMDX({
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    images: {
+        loader: "custom",
+    },
   })
