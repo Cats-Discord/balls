564df37d9258c417b4c935eb6ad6a5e99dc84705
Jackson Isaiah <jacksonisiah01@gmail.com>
2022-10-07 14:16:07 -0400

    Refactor countdown into a react component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import Script from 'next/script';
+import Countdown from '../components/countdown';
 
 export default function Home() {
     return (
         <div className={styles.wrapper}>
-        <Head>
-            <title>BALLS</title>
-            <p className={styles.cd}><script type="text/javascript" src="/static/countdown.js"></script></p>
-        </Head>
+            <Head>
+                <title>BALLS</title>
+            </Head>
+            <main>
+                <p id="cd" className={styles.cd}><Script type="text/javascript" src="/static/countdown.js"></Script></p>
+                
+                <Countdown />
                 <h1 className={styles.wxxvhs} data-text="BALLS RECORDS">BALLS RECORDS</h1>
                 <ul className={styles.centerme}>
                     <Link href="/discography"><span className={styles.subtext} data-text="artists">artists</span></Link>
@@ -17,7 +21,7 @@ export default function Home() {
                     <Link href="https://github.com/cats-discord/balls/"><span className={styles.subtext} data-text="artists">tour</span></Link>
                 </ul>
                 <h5 className={styles.centerme}>brought to you by the cats collective</h5>
-                
+            </main>     
         </div>
-    )
+    );
 }
