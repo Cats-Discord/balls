a3a9d8df04a26aabc1be4bfe63cee72248bf3c6e
Jackson Isaiah <jacksonisiah01@gmail.com>
2022-10-07 15:13:07 -0400

    meta 2: electric boogaloo

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,13 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>artist / BALLSRECORDS</title>
+        <link rel="icon" href="https://cdn.discordapp.com/emojis/849891216525885460.gif" type="image/gif" ></link>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://balls.catfile.me" />
+        <meta property="og:title" content="BALLS Record Label" />
+        <meta property="og:description" content="the best record label ever (no contest)" />
+        <meta property="og:image" content="https://cats-discord.github.io/balls21/static/fastmerging.gif" />
+        <meta name="theme-color" content="#ff0066"></meta>
       </Head>
       <main className={styles.backdrop}>{children}</main>
     </>
