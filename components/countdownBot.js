2b4da76b39db75b5d52ccc9a3804c66f8aa2ac82
sushi <s4schx@gmail.com>
2022-10-11 18:08:47 +0200

    Ctrl+V

---
+++ components/countdownBot.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import styles from "../styles/Home.module.css";
+
+class CountdownBot extends Component {
+    constructor(props) {
+        super(props);
+        let time = this.getTimeLeft();
+        this.state = {
+            days: time.days,
+            hours: time.hours,
+            minutes: time.minutes
+        };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState(this.getTimeLeft());
+        }, 1000);
+    }
+
+    getTimeLeft() {
+        const time = Date.parse("November 01, 2022") - Date.parse(new Date());
+        if (time < 0) {
+            return {days: 0, hours: 0, minutes: 0};
+        } else {
+            const minutes = Math.floor((time /1000 / 60) % 60);
+            const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+            const days = Math.floor(time / (1000 * 60 * 60 * 24));
+            
+            return { days, hours, minutes };
+        }
+    } 
+
+    render() {
+        return (<h3 className={styles.cdb}><marquee behavior="alternate" scrollamount="10">Only {this.state.days} days, {this.state.hours} hours, and {this.state.minutes} minutes left!</marquee></h3>);
+    }
+}
+
+export default CountdownBot;
